c03baaa3124913fbc806953ef43e220a49a67c4a
jon <jonacappella@gmail.com>
2024-09-30 22:05:47 -0600

    update to maestro

--- cmd/maestro/main.go
+++ cmd/maestro/main.go
@@ -1,18 +1,36 @@
-package maestro
+package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "Name",
+	Use:     "maestro",
 	Version: "0.1.0",
-	Short:   "How engineers learn about CLIs",
+	Short:   "Orchestrate your workspace like a maestro",
+}
+
+var pullCmd = &cobra.Command{
+	Use:     "pull",
+	Short:   "Pulls the latest changes from the remote repositories",
+	Example: "maestro pull",
+	Args: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("Hello from get command")
+
+		return nil
+	},
 }
 
 func main() {
+	rootCmd.AddCommand(pullCmd)
+
 	err := rootCmd.Execute()
+
 	if err != nil {
 		os.Exit(1)
 	}
